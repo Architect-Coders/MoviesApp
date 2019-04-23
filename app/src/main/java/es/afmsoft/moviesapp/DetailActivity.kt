ee2fe01477ad8f4dc0565d3131866cda25bcf855
angel.marcos <angel.marcos@afmsoft.es>
2019-04-23 12:18:34 +0200

    MOV-4: Pantalla de detalle
    https://jira.afmsoft.local/browse/MOV-4

---
+++ app/src/main/java/es/afmsoft/moviesapp/DetailActivity.kt
@@ -0,0 +1,12 @@
+package es.afmsoft.moviesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
