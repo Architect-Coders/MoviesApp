ccfaeae6eaf0a37c2d252bb76fbf5e9f976f0baf
angel.marcos <angel.marcos@afmsoft.es>
2019-08-14 19:10:10 +0200

    Informes de cobertura y Sonar

--- app/src/test/java/es/afmsoft/moviesapp/ExampleUnitTest.kt
+++ app/src/test/java/es/afmsoft/moviesapp/ExampleUnitTest.kt
@@ -1,6 +1,8 @@
 package es.afmsoft.moviesapp
 
+import es.afmsoft.domain.Movie
 import org.junit.Test
+import es.afmsoft.moviesapp.ui.model.Movie as UiMovie
 
 import org.junit.Assert.*
 
@@ -14,4 +16,15 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
-}
+
+    @Test
+    fun movieClassTest() {
+        var movieData = Movie(1, "movie", "backdrop", "poster", false, "overview")
+        assertEquals("movie", movieData.title)
+    }
+
+    @Test
+    fun moviesClassTest() {
+        var movieData = UiMovie(1, "movie", "backdrop", "poster", "overview")
+        assertEquals("movie", movieData.title)
+    }}
