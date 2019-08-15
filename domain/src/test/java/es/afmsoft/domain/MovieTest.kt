2555b526a49ffd5dd71d1eb0d6a83b7e90278d4e
angel.marcos <angel.marcos@afmsoft.es>
2019-08-15 23:14:11 +0200

    Sonar

---
+++ domain/src/test/java/es/afmsoft/domain/MovieTest.kt
@@ -0,0 +1,12 @@
+package es.afmsoft.domain
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class MovieTest {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
